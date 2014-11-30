23cc66a8ac6d1188fc118d8388e0a7a6a117fb45
Amanpreet Singh <apsdehal@gmail.com>
2014-11-30 13:10:45 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+*
+* This class helps to easily build up events for dynamically generated elements and will have a name 'o'
+*
+**/
+
+var $ = require('jquery');
+
+module.exports = function(element) {
+	return {
+		/**
+		 * This takes an element and callback and delegates it to click event via body element
+		 *
+		 * @param string element Element to which an event has to be attached
+		 * @param callback function Callback function to be called once event is successfuly triggered
+		 */	
+		click: function(callback) {	
+			$('body').on('click', element, callback);
+		}
+	};
+};
